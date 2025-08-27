a1d15c0471e2ebc3cfcea65d3b3b00c0f7674722
Ell Peck <sherryllelliott@gmail.com>
2025-08-27 13:12:48 +0900

    removes event handlers that arent needed on dashboard buttons

--- lib/parsely_web/live/dashboard_live.ex
+++ lib/parsely_web/live/dashboard_live.ex
@@ -2,7 +2,6 @@ defmodule ParselyWeb.DashboardLive do
   use ParselyWeb, :live_view
 
   alias Parsely.BusinessCards
-  alias Parsely.BusinessCards.BusinessCard
 
   def mount(_params, _session, socket) do
     user = socket.assigns.current_user
@@ -15,32 +14,25 @@ defmodule ParselyWeb.DashboardLive do
      )}
   end
 
-  def handle_event("scan-card", _params, socket) do
-    {:noreply, push_navigate(socket, to: ~p"/scan-card")}
-  end
 
-  def handle_event("view-business-cards", _params, socket) do
-    {:noreply, push_navigate(socket, to: ~p"/business-cards")}
-  end
 
   def render(assigns) do
     ~H"""
     <div class="mx-auto max-w-7xl">
       <div class="mb-8">
-        <h1 class="text-2xl font-bold text-gray-900 mb-4">Dashboard</h1>
 
         <div class="flex space-x-4">
           <!-- Scan Business Card -->
-          <.button_primary phx-click="scan-card" class="inline-flex items-center">
+          <.button_link navigate={~p"/scan-card"} class="inline-flex items-center">
             <.icon name="hero-camera" class="h-4 w-4 mr-2" />
             Scan Business Card
-          </.button_primary>
+          </.button_link>
 
           <!-- View Business Cards -->
-          <.button_secondary phx-click="view-business-cards" class="inline-flex items-center">
+          <.button_link_secondary navigate={~p"/business-cards"} class="inline-flex items-center">
             <.icon name="hero-eye" class="h-4 w-4 mr-2" />
             View Business Cards
-          </.button_secondary>
+          </.button_link_secondary>
         </div>
       </div>
     </div>
